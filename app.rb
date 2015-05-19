7cca7bc09b477f5c19f70bc07c653547ce9ea161
Drew <drew@finstrom.us>
2015-05-18 21:15:18 -0700

    updated test query to return flavor label names instead of ids; assign specific color values to pie chart

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ get('/') do
   chocolate = Flavor.create({name: 'Chocolate'})
   vanilla = Flavor.create({name: 'Vanilla'})
   grape = Flavor.create({name: 'Grape'})
+  strawberry = Flavor.create({name: 'Strawberry'})
 
   Person.create({name: 'Sue', flavor_id: chocolate.id})
   Person.create({name: 'Bill', flavor_id: chocolate.id})
@@ -26,8 +27,12 @@ get('/') do
   Person.create({name: 'Suresh', flavor_id: vanilla.id})
   Person.create({name: 'Andy', flavor_id: vanilla.id})
   Person.create({name: 'Jane', flavor_id: vanilla.id})
+  Person.create({name: 'Sun', flavor_id: vanilla.id})
   Person.create({name: 'James', flavor_id: grape.id})
   Person.create({name: 'Ryan', flavor_id: grape.id})
+  Person.create({name: 'Vera', flavor_id: grape.id})
+  Person.create({name: 'Elanor', flavor_id: strawberry.id})
+  Person.create({name: 'Biff', flavor_id: strawberry.id})
 
   erb(:pie)
 end
